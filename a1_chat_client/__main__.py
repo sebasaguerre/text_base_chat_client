c70f9d203082417c50c1afaa647769d16f1f5135
Sebastian Aguerre <agd.sebastian@gmail.com>
2026-05-15 18:22:05 +0200

    V1 chat service, pre testing

--- a1_chat_client/__main__.py
+++ a1_chat_client/__main__.py
@@ -1,6 +1,7 @@
 from argparse import Namespace, ArgumentParser
 import socket
 import select
+import sys 
 
 FORBIDDEN_CHARS = set("!@#$%^&* ")
 
@@ -83,17 +84,64 @@ def recv_line(sock, buffer):
     
     return line
 
-
-
 def handle_server_msg(line):
-    pass
+    """
+    check for every server response interpret them
+    """
+    if line.startswith("DELIVERY "):
+        parts = line.splits(" ", 2)
+        if len(parts) == 3:
+            print(f"From {parts[1]}: {parts[2]}")
+
+    elif line == "SEND-OK":
+        print("The message was sent succesfully")
+
+    elif line == "BAD-DEST-USER":
+        print("The destination user does not exist")
+
+    elif line.startswith("LIST-OK "):
+        user_list_str = line[len("LIST-OK "): ]
+        users = user_list_str.split(",") if user_list_str else [] 
+        print(f"There are {len(users)} online users: ")
+        for user in users:
+            print(user)
+
+    elif line == "BAD-RQST-HDR":
+        print("Error: Unknown issue in previous message header.")
+ 
+    elif line == "BAD-RQST-BODY":
+        print("Error: Unknown issue in previous message body.")
+ 
+    return True
 
 def handle_user_input(sock, line):
-    pass
+    "Parse a single line of user input and act accordingly"
+    
+    line = line.strip()
+
+    # exit chat 
+    if line == "!quit":
+        return False
+    
+    # recieve list of currently active users 
+    elif line == "!who":
+        send_msg(sock, "LIST")
+    
+    # user and message processing
+    elif line.startwith("@"):
+        # @[usrname] [message]
+        parts = line.split(" ", 1)
+        # send message 
+        if len(parts) == 2:
+            dest_user = parts[0][1:]
+            msg = parts[1]
+            send_msg(sock, f"SEND {dest_user} {msg}")
+
+    # ignore input that does not follow the above commands 
 
 # authentification and login functions 
 
-def contain_forbidden_chars(name):
+def contains_forbidden_chars(name):
     "Check if usarename contains any forbidden chars"
     return any(char in FORBIDDEN_CHARS for char in name)
 
@@ -103,10 +151,43 @@ def login(sock, buffer):
     Returns True if succesful login or False otherwise
     """
 
-    print("Welcome to Chat Client. Enter your login: ", end="", flush=True)
-    
+    # print("Welcome to Chat Client. Enter your login: ", end="", flush=True)
 
+    # loop until login happens, no response or Busy
+    while True:
+            
+            print("Welcome to Chat Client. Enter your login: ", end="", flush=True)
+            username = input().strip()
 
+            # check for forbbiden char
+            if contains_forbidden_chars(username):
+                print(f"Cannot log in as {username}. That username contains disallowed characters.")
+                continue
+            
+            # send msg to server
+            send_msg(sock, f"HELLO-FROM {username}")
+            response = recv_line(sock)
+
+            if response is None:
+                return False 
+            
+            # validate login 
+            if response == f"HELLO {username}":
+                print(f"Succefully logged in as {username}!")
+                return True 
+            
+            # user in use
+            elif response == "IN-USE":
+                print(f"Cannot log in as {username}. That username is already in use.")
+
+            # server is busy, terminate interaction
+            elif response == "BUSY":
+                print("Cannot log in. The server is full!")
+                return False 
+            
+            else: 
+                print(f"Cannot log in as {username}. That username contains disallowed characters.")
+                
 
 # Execute using `python -m a1_chat_client`
 def main() -> None:
@@ -124,10 +205,45 @@ def main() -> None:
     # connect socket
     sock.connect((host, port))
 
+    # buffer to monitor multiple files/interactions
+    buffer = []
 
-  
+    # check if lockin is not succesful 
+    if not login(sock, buffer):
+        sock.close()
+        return 
+    
+    # server-client interaction until smt fails or 
+    while True:
 
+        readable, _, _ = select.select([sock, sys.stdin])
 
+        for fd in readable:
+            
+            # check file is socket 
+            if fd is sock: 
+                line = recv_line(sock)
+
+                if line is None:
+                    print("Disconnected from server.")
+                    sock.close()
+                    return
+
+                # process server msg
+                handle_server_msg(line)
+            
+            elif fd is sys.stdin():
+                user_line = sys.stdin.readline()
+
+                if not user_line:
+                    sock.close()
+                    return 
+                
+                if not handle_user_input(sock, user_line):
+                    sock.close()
+                    return 
+
+    
 if __name__ == "__main__":
     main()
 
